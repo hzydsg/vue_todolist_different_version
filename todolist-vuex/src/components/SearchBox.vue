b7f8c606f0cdcc8829ee8d7a410109d67c2d5b44
hzydsg <84256647+hzydsg@users.noreply.github.com>
2022-10-03 03:38:18 +0800

    todolist_vuex
    使用Vue2官方推荐的主流的存储库来实现任意组件的通信，从而实现todolist

---
+++ todolist-vuex/src/components/SearchBox.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="SearchBox">
+        <input type="text" placeholder="请输入你的任务名称,按下回车确认" v-model="todoListName" @keydown.enter="enterName">
+    </div>
+</template>
+
+<script>
+export default {
+    name:"SearchBox",
+    data(){
+        return {
+            todoListName:""
+        }
+    },
+    methods:{
+        enterName(){
+           this.$store.dispatch("addTodo",this.todoListName);
+           this.todoListName = "";
+        }
+    }
+}
+</script>
+
+<style scoped>
+    input[type="text"]{
+        -web-kit-appearance:none;
+        -moz-appearance: none;
+        outline: 0;
+        width: 100%;
+        border: 2px solid #F0F0F0;
+        height: 40px;
+        padding: 10px;
+        box-sizing: border-box;
+        border-radius: 5px;
+    }
+
+    input[type="text"]:active{
+        border: 2px solid #F0F0F0;
+    }
+</style>
