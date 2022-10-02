c6b72d07397513aa83b0d194e0dea3b0ade47cdf
hzydsg <84256647+hzydsg@users.noreply.github.com>
2022-10-03 03:15:08 +0800

    vue_todolist_props
    在Vue中使用props实现todolist

---
+++ todolist/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+      <search-box :todoLists="todoLists"></search-box>
+      <my-list :todoLists="todoLists" :checkedTodos="checkedTodos"></my-list>
+      <data-statistics :todoLists="todoLists" :checkedTodos="checkedTodos"></data-statistics>
+  </div>
+</template>
+
+<script>
+import searchBox from './components/SearchBox.vue';
+import myList from './components/MyList.vue';
+import dataStatistics from './components/DataStatistics.vue';
+import dayjs from "dayjs";
+export default {
+  name: 'App',
+  data(){
+    return {
+      todoLists:[
+        {
+          name:"编程"
+        },
+        {
+          name:"唱歌"
+        },
+        {
+          name:"打游戏"
+        }
+      ],
+      checkedTodos:[]
+    }
+  },
+  components: {
+    SearchBox:searchBox,
+    MyList:myList,
+    DataStatistics:dataStatistics
+  },
+  created(){
+    this.todoLists = this.todoLists.map((item,index)=>{
+      item.id = dayjs(Date.now()+index*1000).format("YYYYMMDDHHmmssSSS");
+      return item;
+    })
+  }
+}
+</script>
+
+<style>
+  #app {
+      width: 600px;
+      border: 3px solid #F0F0F0;
+      border-radius: 5px;
+      padding: 20px;
+  }
+</style>
